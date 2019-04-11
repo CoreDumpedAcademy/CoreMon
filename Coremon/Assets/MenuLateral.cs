aecb01012646ca76717fe9c6bd0157a76c3e94a1
luislombardis <anguilaelectrica3@gmail.com>
2019-04-11 03:10:47 +0200

    Script Menu lateral

---
+++ Coremon/Assets/MenuLateral.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLateral : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject MenuLateralUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Close();
+            }
+            else
+            {
+                Open();
+            }
+        }
+    }
+
+    public void Close()
+    {
+        MenuLateralUI.SetActive(false);
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+    }
+    public void Open()
+    {
+        MenuLateralUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Coredex()
+    {
+        //activar UI equipo y desactivar UI actual
+    }
+
+    public void Team()
+    {
+        //activar UI equipo y desactivar UI actual
+    }
+
+    public void Bag()
+    {
+        //activar UI mochila y desactivar UI actual
+    }
+
+    public void Quit()
+    {
+        //Cargar escena inicio
+    }
+}
