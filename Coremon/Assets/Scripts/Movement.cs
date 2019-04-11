f750264bf435423c858429af4205aafea21faef7
unknown <lm.lombardo@alumnos.upm.es>
2019-04-11 16:55:20 +0200

    Solved conflicts

--- Coremon/Assets/Scripts/Movement.cs
+++ Coremon/Assets/Scripts/Movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float gridSize = 1f;
-    float moveSpeed = 0.2f;
+    public float moveSpeed = 0.2f;
 
     private Vector3 destination;
     private Vector3 dir;
@@ -18,7 +18,6 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-<<<<<<< HEAD
 
         if(dir == Vector3.zero)
         {
@@ -44,21 +43,6 @@ public class Movement : MonoBehaviour
                 dir = Vector3.down;
             }
         } else
-=======
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position = new Vector2(transform.position.x + move, transform.position.y);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            transform.position = new Vector2(transform.position.x - move, transform.position.y);
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y + move);
-        }
-        else if (Input.GetKey(KeyCode.S))
->>>>>>> 3f35cc2077f52e611d1985e2230546578df34df8
         {
             //Move if stored direction
 
