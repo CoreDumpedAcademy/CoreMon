edac27c719a4290e2c9ca8a5637624ae2eada564
luislombardis <anguilaelectrica3@gmail.com>
2019-04-12 01:51:54 +0200

    Eleccion aleatoria de entre los 30 pokemon

--- Coremon/Assets/Coremonsalvaje.cs
+++ Coremon/Assets/Coremonsalvaje.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Coremonsalvaje : MonoBehaviour
 {
+    public int coremon = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        coremonNuevo();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void coremonNuevo()
+    {
+        int coremon = Random.Range(0, 29);
     }
 }
