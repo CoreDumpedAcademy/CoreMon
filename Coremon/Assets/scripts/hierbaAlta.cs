e75e0da4b714ed37f7f36629ca0509006b7881ea
Flaviu Bogdan Condor <jaguaru40@yahoo.es>
2019-04-12 00:10:25 +0200

    Testing coliders

--- Coremon/Assets/scripts/hierbaAlta.cs
+++ Coremon/Assets/scripts/hierbaAlta.cs
@@ -6,7 +6,7 @@ public class hierbaAlta : MonoBehaviour
 {
     float probCoremon = 0;
     
-    private void OnTriggerEnter2D(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         probCoremon = Mathf.Round(Random.Range(0f,1000f));
 
