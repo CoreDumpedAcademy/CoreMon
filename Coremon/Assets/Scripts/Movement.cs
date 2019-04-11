4555769779e86095cfcb5f47faae3b64ce5a53e7
unknown <leian7957@gmail.com>
2019-04-11 13:30:19 +0200

    Modified movement to adjust to grid

--- Coremon/Assets/Scripts/Movement.cs
+++ Coremon/Assets/Scripts/Movement.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float move = 0.1f;
-    
+    public float gridSize = 1f;
+    float moveSpeed = 0.2f;
+
+    private Vector3 destination;
+    private Vector3 dir;
+    private Vector3 distance = Vector3.zero;
 
     void Start()
     {
@@ -14,21 +18,68 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.D))
+
+        if(dir == Vector3.zero)
         {
-            transform.position = new Vector2(transform.position.x + move, transform.position.y);
-        }
-        if (Input.GetKey(KeyCode.A))
+            //Get the direction to move
+            if (Input.GetKey(KeyCode.D))
+            {
+                destination = transform.position + Vector3.right * gridSize;
+                dir = Vector3.right;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                destination = transform.position + Vector3.left * gridSize;
+                dir = Vector3.left;
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                destination = transform.position + Vector3.up * gridSize;
+                dir = Vector3.up;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                destination = transform.position + Vector3.down * gridSize;
+                dir = Vector3.down;
+            }
+        } else
         {
-            transform.position = new Vector2(transform.position.x - move, transform.position.y);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y + move);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y - move);
+            //Move if stored direction
+
+            distance = destination - transform.position; //vector distance
+            transform.Translate(dir * moveSpeed);
+            if (dir == Vector3.right)
+            {
+                if (distance.x < moveSpeed)
+                {
+                    dir = Vector3.zero;
+                    transform.position = destination;
+                }
+            }
+            else if (dir == Vector3.left)
+            {
+                if (distance.x > -moveSpeed)
+                {
+                    dir = Vector3.zero;
+                    transform.position = destination;
+                }
+            }
+            else if (dir == Vector3.down)
+            {
+                if (distance.y > -moveSpeed)
+                {
+                    dir = Vector3.zero;
+                    transform.position = destination;
+                }
+            }
+            else if (dir == Vector3.up)
+            {
+                if (distance.y < moveSpeed)
+                {
+                    dir = Vector3.zero;
+                    transform.position = destination;
+                }
+            }
         }
     }
 }
