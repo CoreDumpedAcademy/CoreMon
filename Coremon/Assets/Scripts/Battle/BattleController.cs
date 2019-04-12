1e54a5374bb9a4bb9b2ca53e9e9f5a94a8a53ab5
unknown <leian7957@gmail.com>
2019-04-12 12:18:58 +0200

    Added functionality to MenuController

--- Coremon/Assets/Scripts/Battle/BattleController.cs
+++ Coremon/Assets/Scripts/Battle/BattleController.cs
@@ -4,101 +4,28 @@ using UnityEngine;
 
 public class BattleController : MonoBehaviour
 {
-    PointerMaster pointer;
-    menuOptions currOption = menuOptions.Atacar;
-    menuInput keyInput = menuInput.None;
-    void Start()
+    menuOptions action = menuOptions.None;
+    MenuController menu;
+    private void Start()
     {
-        //Get the pointer master script from the PointerMaster child object
-        pointer = transform.Find("PointerMaster").gameObject.GetComponent<PointerMaster>();
+        menu = gameObject.GetComponent<MenuController>();
     }
-
     void Update()
     {
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            keyInput = menuInput.Left;
-        } else if (Input.GetAxis("Horizontal") > 0)
-        {
-            keyInput = menuInput.Right;
-        } else if (Input.GetAxis("Vertical") < 0)
-        {
-            keyInput = menuInput.Down;
-        } else if (Input.GetAxis("Vertical") > 0)
-        {
-            keyInput = menuInput.Up;
-        }
-
-        Debug.Log(keyInput);
-
-        if(keyInput != menuInput.None)
-        {
-
-            switch (currOption)
-            {
-                case menuOptions.Atacar:
-                    if (keyInput == menuInput.Right) updateOption(menuOptions.Cambiar);
-                    if (keyInput == menuInput.Down) updateOption(menuOptions.Capturar);
-
-                    if(keyInput == menuInput.Yes)
-                    {
-                        //Add function call
-                    }
-
-                    break;
-                case menuOptions.Capturar:
-                    if (keyInput == menuInput.Right) updateOption(menuOptions.Huir);
-                    if (keyInput == menuInput.Up) updateOption(menuOptions.Atacar);
-
-                    if (keyInput == menuInput.Yes)
-                    {
-                        //Add function call
-                    }
-
-                    break;
-                case menuOptions.Huir:
-                    if (keyInput == menuInput.Left) updateOption(menuOptions.Capturar);
-                    if (keyInput == menuInput.Up) updateOption(menuOptions.Cambiar);
-
-                    if (keyInput == menuInput.Yes)
-                    {
-                        //Add function call
-                    }
-
-                    break;
-                case menuOptions.Cambiar:
-                    if (keyInput == menuInput.Left) updateOption(menuOptions.Atacar);
-                    if (keyInput == menuInput.Down) updateOption(menuOptions.Huir);
-
-                    if (keyInput == menuInput.Yes)
-                    {
-                        //Add function call
-                    }
-
-                    break;
-            }
-
-            keyInput = menuInput.None;
-        }
-        
-
-
-
+        action = menu.getActions();
+        Debug.Log(action);
     }
 
-    void updateOption(menuOptions option)
-    {
-        pointer.selectOption(option);
-        currOption = option;
-    }
+
 
     public enum menuOptions
     {
+        None,
         Atacar,
         Capturar,
         Huir,
         Cambiar
     }
-    
-    enum menuInput {None, Right, Left, Up, Down, Yes, No}
+
+
 }
