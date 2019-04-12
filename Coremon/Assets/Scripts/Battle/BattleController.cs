5d38cbb2ea9d6ab2b642f547cdabaeb2fb7d6233
unknown <leian7957@gmail.com>
2019-04-12 12:27:18 +0200

    Added more comments

--- Coremon/Assets/Scripts/Battle/BattleController.cs
+++ Coremon/Assets/Scripts/Battle/BattleController.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class BattleController : MonoBehaviour
 {
-    menuOptions action = menuOptions.None;
-    MenuController menu;
+    menuOptions action = menuOptions.None;     //Action the Battle controller will do next
+    MenuController menu;                       //Menu controller has all the functions that controll the menu
     private void Start()
     {
-        menu = gameObject.GetComponent<MenuController>();
+        menu = gameObject.GetComponent<MenuController>();     //Initializing menu controller script
     }
     void Update()
     {
-        action = menu.getActions();
+        action = menu.getActions();                   //Calling the menu controller to open the menu and get an action
         Debug.Log(action);
+
+        //Do stuff according to the actions received
+
     }
 
 
 
+    //Enum listing the actions the controller will take
+    //Referenced by Menu Controller
     public enum menuOptions
     {
         None,
