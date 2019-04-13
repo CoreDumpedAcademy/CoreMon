b8b0663fbf6898f8f59a1b9ccd2102408132f568
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2019-04-13 22:04:41 +0200

    Save function made async

--- Coremon/Assets/Scripts/APIController.cs
+++ Coremon/Assets/Scripts/APIController.cs
@@ -1,8 +1,9 @@
-﻿using System.Net;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net;
 using System.IO;
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class APIController : MonoBehaviour
@@ -10,21 +11,26 @@ public class APIController : MonoBehaviour
 
     public UserInfo saveData;
 
-    public string[][] test = new string[10][];
-
     private void Start()
     {
         string user = "nyan3";
 
-        saveData = getSaveData(user);
+        saveData = loadSaveData(user);
 
         Debug.Log(saveData.bag);
     }
 
-    public UserInfo getSaveData(string username)
+
+    public UserInfo loadSaveData(string user)
+    {
+        Task<UserInfo> task = Task.Run<UserInfo>(async () => await getSaveData(user));  //Non scalable solution. Creates new thread. Works for now.
+        return task.Result;
+    }
+
+    public async Task<UserInfo> getSaveData(string username)
     {
         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(string.Format("http://localhost:3000/users/username&{0}", username));
-        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        HttpWebResponse response = (HttpWebResponse)( await request.GetResponseAsync() );
         StreamReader reader = new StreamReader(response.GetResponseStream());
 
         string jsonResponse = reader.ReadToEnd();
