e96a92563bf6921b8e8866ba4bf74b6919232dcd
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2019-04-17 19:13:40 +0200

    Navigation between initial screen, overworld and battle scene

---
+++ Coremon/Assets/Scripts/SceneController.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    //Class with static functions to load different scenes
+    public static void startGame()
+    {
+        //Retrieve save data from Api
+        GameData.playerPos = Vector3Int.zero;
+        loadOverworld();
+    }
+
+    public static void loadOverworld()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("OverworldScene");
+    }
+
+    public static void loadBattle()
+    {
+        GameData.playerPos = Vector3Int.RoundToInt(GameData.player.transform.position);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("BattleScene");
+    }
+}
+
+
+
