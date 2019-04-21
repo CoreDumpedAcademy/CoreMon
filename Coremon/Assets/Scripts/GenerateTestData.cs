13d790a4068517e537a6b00b44042de9561066f2
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2019-04-21 18:49:08 +0200

    Test data generates at Inicio

--- Coremon/Assets/Scripts/GenerateTestData.cs
+++ Coremon/Assets/Scripts/GenerateTestData.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class GenerateTestData : MonoBehaviour
 {
     CoremonController controller;
-    Coremon[] savedTeam = new Coremon[3]; 
+    Coremon[] savedTeam = new Coremon[3];
+    UserInfo savedData;
     void Start()
     {
         controller = gameObject.GetComponent<CoremonController>();
 
         GameData.saveData = new UserInfo();
         GameData.saveData.team = new Coremon[3];
+        GameData.saveData.coredex = new int[30];
+        GameData.saveData.bag = new Item[10];
+        for(int i = 0; i < 30; i++)
+        {
+            GameData.saveData.coredex[i] = 0;
+        }
+
+        GameData.saveData.coredex[0] = 2;
+        GameData.saveData.coredex[1] = 2;
+        GameData.saveData.coredex[2] = 2;
+        GameData.saveData.coredex[5] = 1;
+        GameData.saveData.coredex[15] = 1;
+        GameData.saveData.coredex[29] = 1;
 
         for (int i = 0; i < 3; i++)
         {
@@ -21,14 +35,30 @@ public class GenerateTestData : MonoBehaviour
 
         GameData.saveData.team[0].name = "Fushigidatesto";
         GameData.saveData.team[1].name = "Testardon";
+        GameData.saveData.team[1].NumCoremon = 2;
         GameData.saveData.team[2].name = "El que queda";
+        GameData.saveData.team[2].NumCoremon = 3;
 
-        
+
+        GameData.saveData.username = "TestData";
+        GameData.saveData.money = 100;
+
+        Item item0 = new Item();
+        Item item1 = new Item();
+
+        item0.name = "Coreball";
+        item0.amount = 20;
+        item1.name = "Supercore";
+        item1.amount = 10;
+
+        GameData.saveData.bag[0] = item0;
+        GameData.saveData.bag[1] = item1;
     }
 
     private void Update()
     {
         savedTeam = GameData.saveData.team;
+        savedData = GameData.saveData;
     }
 
 }
