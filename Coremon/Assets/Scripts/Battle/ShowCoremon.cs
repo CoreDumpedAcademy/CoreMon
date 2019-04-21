4014baf8621a0252d6833206a010ac275aaeb6b8
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2019-04-21 17:37:31 +0200

    Added sprite field to coremon. Showing coremon sprite in battle

---
+++ Coremon/Assets/Scripts/Battle/ShowCoremon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowCoremon : MonoBehaviour
+{
+    GameObject Batalla;
+    Atack atackScript;
+    CoremonController controller;
+
+    GameObject cor;
+    Vector3 corScale;
+    GameObject enemyCor;
+
+    GameObject corUI;
+    GameObject enemyCorUI;
+
+    private void Awake()
+    {
+        Batalla = GameObject.Find("Batalla");
+        atackScript = Batalla.GetComponent<Atack>();
+        controller = Batalla.GetComponent<CoremonController>();
+
+        cor = transform.GetChild(0).gameObject;
+        enemyCor = transform.GetChild(1).gameObject;
+        //corUI = transform.GetChild(2).gameObject;
+        //enemyCorUI = transform.GetChild(3).gameObject;
+
+        corScale = cor.GetComponent<RectTransform>().localScale;
+        corScale = new Vector3(corScale.x * -1, corScale.y, corScale.z);
+        cor.GetComponent<RectTransform>().localScale = corScale;
+
+    }
+
+    private void Update()
+    {
+        cor.GetComponent<Image>().sprite = atackScript.cor.sprite;
+        enemyCor.GetComponent<Image>().sprite = atackScript.enemyCor.sprite;
+    }
+}
