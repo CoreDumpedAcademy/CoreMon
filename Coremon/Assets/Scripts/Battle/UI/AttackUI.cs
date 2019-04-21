74639d2b25e9d4d38006bddb506233e94c9050ae
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2019-04-21 19:11:01 +0200

    Show attack text

---
+++ Coremon/Assets/Scripts/Battle/UI/AttackUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttackUI : MonoBehaviour
+{
+
+    Atack atackScript;
+
+    Text at1Text;
+    Text at2Text;
+    Text at3Text;
+    Text at4Text;
+
+    void Start()
+    {
+        atackScript = GameObject.Find("Batalla").GetComponent<Atack>();
+
+        at1Text = transform.GetChild(0).GetComponent<Text>();
+        at2Text = transform.GetChild(1).GetComponent<Text>();
+        at3Text = transform.GetChild(2).GetComponent<Text>(); 
+        at4Text = transform.GetChild(3).GetComponent<Text>();
+    }
+
+
+    void Update()
+    {
+        at1Text.text = atackScript.cor.Atacks[0].name;
+        at2Text.text = atackScript.cor.Atacks[1].name;
+        at3Text.text = atackScript.cor.Atacks[2].name;
+        at4Text.text = atackScript.cor.Atacks[3].name;
+    }
+}
